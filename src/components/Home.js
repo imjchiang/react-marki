0664662332add32a1bafd0583a6bb6b97c871d1f
imjchiang <jchiang296@gmail.com>
2022-07-11 12:59:06 -0700

    [Home] implement placeholder images and links to other paths

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,34 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import placeholder from '../logo.svg';
 import "../App.css"
 
 const Home = () =>
 {
     return(
-        <div>
-            <h1>MarKi Co., Ltd.</h1>
-            <h2>Packaging Solutions</h2>
-            <Link className="" to="/allproducts">
-                Our Products
-            </Link>
-            <p className="">We do boxes and much more!!</p>
-        </div>
+        <>
+            <div>
+                <h1>MarKi Co., Ltd.</h1>
+                <h2>Packaging Solutions</h2>
+            </div>
+            <div>
+                <Link className="" to="/allproducts">
+                    Our Products
+                </Link>
+                <Link to="/tbd1">
+                    <img src={placeholder}/>
+                    Food Packaging Boxes
+                </Link>
+                <Link to="/tbd2">
+                    <img src={placeholder}/>
+                    Food Packagaing Accesories
+                </Link>
+                <Link to="/tbd3">
+                    <img src={placeholder}/>
+                    Personal Care Packaging
+                </Link>
+            </div>
+        </>
     )
 }
 
