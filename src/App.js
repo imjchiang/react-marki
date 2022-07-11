3195220995211ceae4363195c020b826aaa79d6a
imjchiang <jchiang296@gmail.com>
2022-07-11 13:51:11 -0700

    [Navbar] implement navbar and bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import {Routes, Route} from "react-router-dom";
 
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
