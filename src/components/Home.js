379dad69b264f837d13f870a9fb605162edbe3a5
imjchiang <jchiang296@gmail.com>
2022-07-25 16:53:29 -0700

    [Home] more code cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import {motion} from 'framer-motion';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 // components
 import Footer from './Footer.js';
