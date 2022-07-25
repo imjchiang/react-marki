9b45d61385b7075e0bd7100aaaf07b37ff06d6bf
imjchiang <jchiang296@gmail.com>
2022-07-25 11:31:06 -0700

    [App] fix indentation

--- src/App.js
+++ src/App.js
@@ -12,63 +12,65 @@ import './App.css';
 
 function App() 
 {
-  const location = useLocation();
+    const location = useLocation();
 
-  useEffect(() => {
-    console.log('route has been changed' + location.pathname);
-    var fader = document.getElementById('fader');
-    fader.classList.add('fade-out');
-  },[location.pathname]);
+    useEffect(() => {
+        console.log('route has been changed' + location.pathname);
+        var fader = document.getElementById('fader');
+        fader.classList.add('fade-out');
+    },[location.pathname]);
 
-  let foodPack = [
-    {
-      type: "containers",
-      variants: ["paper", "plastic"]
-    },
-    {
-      type: "bottles",
-      variants: ["wine", "others"]
-    },
-    {
-      type: "components",
-      variants: ["stickers", "ribbons", "labels"]
-    }
-  ];
+    let foodPack = 
+    [
+        {
+            type: "containers",
+            variants: ["paper", "plastic"]
+        },
+        {
+            type: "bottles",
+            variants: ["wine", "others"]
+        },
+        {
+            type: "components",
+            variants: ["stickers", "ribbons", "labels"]
+        }
+    ];
 
-  let personalPack = [
-    {
-      type: "containers",
-      variants: ["paper", "plastic"]
-    },
-    {
-      type: "bottles",
-      variants: ["plastic", "glass"]
-    },
-    {
-      type: "components",
-      variants: ["cap", "pump", "labels", "others"]
-    }
-  ];
-  
-  return (
-    <div className='App'>
-      <svg id="fader"></svg>
-      <Navbar />
-      {/* Switch has been replaced with Routes with new react-router-dom version */}
-      {/* all routes are here */}
-      <Routes>
-        <Route 
-          exact 
-          path='/displayproducts' 
-          element={<DisplayProducts foodPack={foodPack} personalPack={personalPack} />}
-        />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/' element={<Home />} />
-        {/* error page displayed if no component is found for the path specified */}
-        <Route path='*' element={ <ErrorPage />} />
-      </Routes>
-    </div>
-  );
+    let personalPack = 
+    [
+        {
+            type: "containers",
+            variants: ["paper", "plastic"]
+        },
+        {
+            type: "bottles",
+            variants: ["plastic", "glass"]
+        },
+        {
+            type: "components",
+            variants: ["cap", "pump", "labels", "others"]
+        }
+    ];
+    
+    return (
+        <div className='App'>
+        <svg id="fader"></svg>
+        <Navbar />
+        {/* Switch has been replaced with Routes with new react-router-dom version */}
+        {/* all routes are here */}
+        <Routes>
+            <Route 
+                exact 
+                path='/displayproducts' 
+                element={<DisplayProducts foodPack={foodPack} personalPack={personalPack} />}
+            />
+            <Route exact path='/contact' element={<Contact />} />
+            <Route exact path='/' element={<Home />} />
+            {/* error page displayed if no component is found for the path specified */}
+            <Route path='*' element={ <ErrorPage />} />
+        </Routes>
+        </div>
+    );
 }
 
 export default App;
