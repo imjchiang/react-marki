06a5f3e2c68c2bc18af0b0d730ceb493828cb8f7
imjchiang <jchiang296@gmail.com>
2022-07-25 17:05:44 -0700

    [ErrorPage] clean up code, implement framer-motion

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,22 +1,30 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
-const Home = () =>
+// css
+
+const ErrorPage = () =>
 {
     return(
-        <div>
-            <h1 class="sub-title">Error 404: Page Not Found :(</h1>
-            <p class="info">Sorry, the page you have requested is not available. If you believe this was an error on our part, please contact our web development team!</p>
-            <form action="/contact" method="get" class="contact-button-form">
-                <button type="submit" class="card category-button">
-                    <h5 class="card-title">Contact Us!!</h5>
+        <motion.div
+            key='error'
+            initial={{opacity:0}}
+            animate={{opacity:1, transition:{duration:1}}}
+            exit={{opacity:0, transition:{duration:1}}}>
+
+            <h1 class='sub-title'>Error 404: Page Not Found :(</h1>
+            <p class='info'>Sorry, the page you have requested is not available. If you believe this was an error on our part, please contact our web development team!</p>
+            <form action='/contact' method='get' class='contact-button-form'>
+                <button type='submit' class='card category-button'>
+                    <h5 class='card-title'>Contact Us!!</h5>
                 </button>
             </form>
-            <Link className="error-home" to="/">
+            <Link className='error-home' to='/'>
                 Home
             </Link>
-        </div>
+        </motion.div>
     )
 }
 
-export default Home;
+export default ErrorPage;
