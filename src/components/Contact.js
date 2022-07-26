3fde22f5843a84a05af53ec28a09de3b73621d34
imjchiang <jchiang296@gmail.com>
2022-07-25 17:08:15 -0700

    [Contact] clean up code, implement framer-motion

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React from 'react';
+import { motion } from 'framer-motion';
+
+// components
+import Footer from './Footer.js';
+
+// css
 
 const Contact = () =>
 {
     return(
-        <div>
-            <h1 className="sub-title">Contact Us</h1>
+        <motion.div
+            key='contact'
+            initial={{opacity:0}}
+            animate={{opacity:1, transition:{duration:1}}}
+            exit={{opacity:0, transition:{duration:1}}}>
+
+            <h1 className='sub-title'>Contact Us</h1>
             <p>t: +886 7 394 7011</p>
             <p>f: +886 7 394 4753</p>
             <p>t TW: +886 929 576840</p>
@@ -15,7 +26,10 @@ const Contact = () =>
 
             <h2>Stephanie Yeh</h2>
             <p>Email: stephanie@marki.com.tw</p>
-        </div>
+
+            <Footer />
+            
+        </motion.div>
     )
 }
 
