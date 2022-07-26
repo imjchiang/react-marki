bc322a5c2f613293df4ce99834b40922647370aa
imjchiang <jchiang296@gmail.com>
2022-07-25 18:25:33 -0700

    [index] transition to react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+const container = document.getElementById("root");
+const rootContainer = ReactDOM.createRoot(container);
+rootContainer.render(
+    // <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    // </React.StrictMode>
+)
